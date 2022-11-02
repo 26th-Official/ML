e2a47aea5503939c1304cdb8a0bbd7553532151b
26th-Official <you@example.com>
2022-11-02 16:49:56 +0530

    Yt

--- ML.py
+++ ML.py
@@ -1,14 +1,8 @@
-import numpy as np
- 
-# softmax_output = np.array([0.7,0.2,0.3],
-#                         [0.1,0.4,0.5]
-#                         [0.09,0.8,0.2])
+from pytube import Playlist
+p = Playlist('https://www.youtube.com/playlist?list=PLQVvvaa0QuDfSfqQuee6K8opKtZsh7sA9')
 
-# class_target = [1,0,0]
-
-
-# print(softmax_output[range(len(softmax_output)),class_target])
-
-bias = np.zeros((1,5))
-print(bias)
- 
+count = 1
+for video in p.videos:
+    print(f'Downloading: {count}')
+    video.streams.filter(res="720p").first().download()
+    count+=1
