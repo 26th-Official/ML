677bcb321deca900b44f3b94007f5c769cca3176
26th_Official <26thofficial.creator@gmail.com>
2022-11-02 15:57:54 +0530

    V1

--- DA - sendex.py
+++ DA - sendex.py
@@ -2,13 +2,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("avocado.csv")
+df = df[df["type"]=="organic"]
 df["Date"] = pd.to_datetime(df["Date"])
+# df.set_index("Date",inplace=True)
+# df.sort_index(inplace=True)
+print(df["region"].unique())
 
-region = df[df["region"]== "Albany"]
+# graph_df = pd.DataFrame()
 
-region.set_index("Date",inplace=True)
-region.sort_index(inplace=True)
-region["AveragePrice"].rolling(25).mean().plot()
+# for i in df["region"].unique()[:16]:
+#     print(i)
+#     region = df.copy()[df["region"] == i]
+#     region.sort_index(inplace=True)
+#     region[f"{i}_Avg-price"] = region["AveragePrice"].rolling(25).mean()
+
+#     if graph_df.empty:
+#         graph_df = region[[f"{i}_Avg-price"]]
+#     else:
+#         graph_df = graph_df.join(region[f"{i}_Avg-price"])
+
+new = pd.DataFrame()
+
+for name,group in df.groupby("region"):
+    if new.empty:
+        new = group.set_index("Date")[["AveragePrice"]].rename(columns={"AveragePrice":name})
+    else:
+        new = new.join(group.set_index("Date")[["AveragePrice"]].rename(columns={"AveragePrice":name}))
+
+print(new)
+new.to_csv("sam.csv")
+
+# graph_df.dropna(inplace=True)   
+# graph_df.to_csv("sam.csv")
 
 
+
+# df.plot()
 plt.show()
+# print(df)
+
+
+
+
+
+
+
