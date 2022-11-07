ffa8bd36c3841de4ff8ac52594f8715214d863bc
26th_Official <26thofficial.creator@gmail.com>
2022-11-07 20:58:21 +0530

    V6

--- Tensorflow/Ml_Oreily.py
+++ Tensorflow/Ml_Oreily.py
@@ -2,11 +2,25 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("Datasets/housing.csv")
 print(data.head())
-print(data.info())
+# print(data.info())
 
-x_train,x_test = train_test_split(data,test_size=0.2,random_state=42)
-print(x_train)
-print(x_test)
+
+data["income_category"] = pd.cut(data["median_income"],
+                                 labels=[1,2,3,4,5],
+                                 bins=[0.0,1.0,3.0,5.0,6.0,np.inf])
+
+x_train,x_test = train_test_split(data,test_size=0.2,random_state=42,stratify=)
+# print(x_train)
+# print(x_test)
+
+# print(len(x_train))
+# print(len(x_test))
+
+
+
+
+
